ac99f58fcb1902c62e5a6d58e14f2c6f257e26ba
Ki Sar <rxs291@yahoo.com>
2023-06-06 09:12:21 -0500

    working quiz, need front page start button

--- ki'sFolder/testing/src/components/Quiz.js
+++ ki'sFolder/testing/src/components/Quiz.js
@@ -1,11 +1,13 @@
 import AuthBox from "./AuthBox"
-import { Typography } from '@mui/material';
+import { Typography, Input } from '@mui/material';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 import Button from '@mui/material/Button';
 
@@ -44,72 +46,135 @@ const Quiz = () => {
 
     const [selectedValue, setSelectedValue] = useState('');
     const [activeQuestion, setActiveQuestion] = useState(0);
+    const [defaultText, setDefaultText] = useState('Type Here!');
 
 
 
-    const handleFormSubmit = (e) => { 
-        e.preventDefault(); 
+    ////only using this until production, no reason to clear it out otherwise
+    useEffect(() => {  
+        localStorage.clear();
+      }, []); 
+
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
 
         ///THIS NEEDDS TO BE ERASED
-        console.log(selectedValue);  
+        console.log(selectedValue);
         console.log(quiz[activeQuestion].input)
 
-        //capture inputs and information, if nothin selected, default to 'n/a'
-        if (!selectedValue) { 
-            const defaultValue = options[0][Object.keys(options[0])[0]];
-            localStorage.setItem('userInfo', defaultValue)   
-          } else {
-            localStorage.setItem('userInfo', selectedValue) 
-          }
- 
+        //capture inputs and information, if nothin selected, default to 'n/a' 
+        // const defaultValue = {value: options[0][Object.keys(options[0])[0]]}; 
+        var response = '';
+        response = {
+            question: activeQuestion,
+            answer: selectedValue
+        } 
+        var newInfo = JSON.parse(localStorage.getItem("userInfo")) || [];
+        newInfo.push(response);
+        localStorage.setItem("userInfo", JSON.stringify(newInfo));
+
+        setSelectedValue('')
+
         ///check to see if quiz array is complete
         if (activeQuestion !== quiz.length - 1) {
             setActiveQuestion((prev) => prev + 1)
-          } else {
+        } else {
             ///quiz is over, render new page
             setActiveQuestion(0)
             console.log('quiz complete!')
-          } 
+        }
+
+
     };
+
+    const addLeadingZero = (number) => (number > 9 ? number : `0${number}`)
 
 
     return (
         <>
-        {}
-            <AuthBox>
-                <Typography variant='h5' sx={{ color: 'white' }}>
-                    Tell us about your companion!
-                </Typography>
-                <Typography sx={{ color: '#b9bbbe' }}>
-                    <div style={{
-                        display: 'flex',
-                        flexDirection: 'column'
-                    }}>
-                        <div>
-                            <h3>{quiz[0].question}</h3>
-                        </div>
-                        <FormControl>
-                            <RadioGroup
-                                aria-labelledby="demo-radio-buttons-group-label"
-                                defaultValue="None yet"
-                                name="radio-buttons-group"
-                                value={selectedValue}
-                                onChange={(e) => setSelectedValue(e.target.value)} 
-                            >
+            {!quiz[activeQuestion].input ?
+                (
+                    <AuthBox>
+                        <Typography variant='h5' sx={{ color: 'white' }}>
+                            Tell us about your companion!
+                        </Typography>
+                        <Typography sx={{ color: '#b9bbbe' }}>
+                            <div style={{
+                                display: 'flex',
+                                flexDirection: 'column'
+                            }}>
+                                <div>
+                                    <h3>{quiz[activeQuestion].question}</h3>
+                                    <div>
+                                        <span className="active-question-no">{addLeadingZero(activeQuestion + 1)}</span>
+                                        <span className="total-question">/{addLeadingZero(quiz.length)}</span>
+                                    </div>
+                                </div>
+                                <FormControl>
+                                    <RadioGroup
+                                        aria-labelledby="demo-radio-buttons-group-label"
+                                        defaultValue="None yet"
+                                        name="radio-buttons-group"
+                                        value={selectedValue}
+                                        onChange={(e) => setSelectedValue(e.target.value)}
+                                    >
 
-                                {options.map((option, i) => (
+                                        {options.map((option, i) => (
 
-                                    <FormControlLabel key={i} value={Object.values(option)[0]} control={<Radio />} label={Object.values(option)[0]} />
+                                            <FormControlLabel key={i} value={Object.values(option)[0]} control={<Radio />} label={Object.values(option)[0]} />
 
-                                ))}
-                            </RadioGroup>
-                        </FormControl>
-                        <Button type="button" onClick={handleFormSubmit} style={{ marginTop: 50 }} variant="contained">Submit</Button>
+                                        ))}
+                                    </RadioGroup>
+                                </FormControl>
+                                <Button type="button" onClick={handleFormSubmit} style={{ marginTop: 50 }} variant="contained">Submit</Button>
 
-                    </div>
+                            </div>
 
-                </Typography>
-            </AuthBox>
+                        </Typography>
+                    </AuthBox>
+
+                )
+                :
+                (
+                    <AuthBox>
+                        <Typography variant='h5' sx={{ color: 'white' }}>
+                            Tell us about your companion!
+                        </Typography>
+                        <Typography sx={{ color: '#b9bbbe' }}>
+                            <div style={{
+                                display: 'flex',
+                                flexDirection: 'column'
+                            }}>
+                                <div>
+                                    <h3>{quiz[activeQuestion].question}</h3>
+                                    <div>
+                                        <span className="active-question-no">{addLeadingZero(activeQuestion + 1)}</span>
+                                        <span className="total-question">/{addLeadingZero(quiz.length)}</span>
+                                    </div>
+                                </div>
+
+                                <FormControl>
+
+                                    <TextField
+                                        id="filled-multiline-static"
+                                        label="Text"
+                                        multiline
+                                        rows={4}
+                                        defaultValue={defaultText}
+                                        variant="filled"
+                                        value={selectedValue}
+                                        onChange={(e) => setSelectedValue(e.target.value)}
+                                    />
+
+                                </FormControl>
+                                <Button type="button" onClick={handleFormSubmit} style={{ marginTop: 50 }} variant="contained">Submit</Button>
+
+                            </div>
+                        </Typography>
+                    </AuthBox>
+                )
+            }
         </>
     )
 }
