2b9585a67bf726bcabdb21c2bd035c74f363cf89
lkrum <leahkrumholz73@gmail.com>
2023-06-07 15:19:43 -0500

    added header to dashboard page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
 
 export default function Dashboard() {
   return (
     <div>
+      <Header />
       Dashboard
-<h1>Hello, User</h1>
+    <h1>Hello, User</h1>
     </div>
   )
 }
