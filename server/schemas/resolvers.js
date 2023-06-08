39dd708ac05a75427a23474a002329eda301e660
JASaddington <jasaddington@gmail.com>
2023-06-08 16:57:57 -0500

    graphql stuff and tokenization partially done

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,44 @@ const resolvers = {
     products: async () => {
       return await Product.find({});
     }
-  }
+  },
+  // Query: {
+  //   me: async (parent, args, context) => {
+  //     // check if users exist
+  //     if (context.user) {
+  //       const userData = await User.findOne({ _id: context.user._id }).select(
+  //         '-__v -password'
+  //       );
+  //       return userData;
+  //     }
+  //     throw new AuthenticationError('Not logged in.');
+  //   },
+  // },
+
+  // Mutation: {
+  //   login: async (parent, { email, password }) => {
+  //     const user = await User.findOne({ email });
+  //     // check if user exists with email and credentials
+  //     if (!user) {
+  //       throw new AuthenticationError('Incorrect credentials.');
+  //     }
+  //     const correctPassword = await user.isCorrectPassword(password);
+
+  //     // check password
+  //     if (!correctPassword) {
+  //       throw new AuthenticationError('Incorrect credentials.');
+  //     }
+
+  //     const token = signToken(user);
+  //     return { token, user };
+  //   },
+  //   addUser: async (parent, args) => {
+  //     const user = await User.create(args);
+  //     const token = signToken(user);
+
+  //     return { token, user };
+  //   },
+  // }
 };
 
 module.exports = resolvers;
