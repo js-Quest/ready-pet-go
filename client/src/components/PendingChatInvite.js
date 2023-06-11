e2315dbca8627916704305289a1b232c755789e6
JASaddington <jasaddington@gmail.com>
2023-06-11 18:40:24 -0500

    building out chat invitations

--- client/src/components/PendingChatInvite.js
+++ client/src/components/PendingChatInvite.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import { styled } from '@mui/material';
+import PendingInvitationListItem from './PendingInvitationListItem';
+
+const DUMMY_INVITATIONS = [
+  {
+    _id: '1',
+    senderId: {
+      username: 'Aaron',
+      email: 'aaron@gmail.com'
+    }
+  },
+  {
+    _id: '2',
+    senderId: {
+      username: 'Joe',
+      email: 'joe@gmail.com'
+    }
+  },
+]
 
 const MainContainer = styled('div')({
   width: '100%',
@@ -13,7 +31,14 @@ const MainContainer = styled('div')({
 export default function PendingInvitations() {
   return (
     <MainContainer>
-
+      {DUMMY_INVITATIONS.map(invitation =>(
+        <PendingInvitationListItem 
+          key={invitation._id}
+          id={invitation.id}
+          username={invitation.senderId.username}
+          email={invitation.senderId.email}
+        />
+      ))}
     </MainContainer>
   )
 }
