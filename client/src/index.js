a25e113d4563a338328e4395b23cf79e4fb01179
JASaddington <jasaddington@gmail.com>
2023-06-11 19:44:28 -0500

    adding socket.io stuff

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './app/store';
+import store from './store/store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { styled } from "@mui/system";
 import ChatSideBar from '../components/ChatSideBar'
 import ChatFriendsSideBar from '../components/ChatFriendsSideBar';
 import ChatAppBar from '../components/ChatAppBar';
 import Messenger from '../components/Messenger';
+import logout from '../utils/auth'
 
 const Wrapper = styled('div')({
   width: '100%',
@@ -12,6 +13,15 @@ const Wrapper = styled('div')({
 });
 
 export default function ChatPage() {
+  useEffect(() => {
+    const userDetails = localStorage.getItem('id_token');
+
+    if(!userDetails){
+      logout();
+    // }else{
+    //   setUserDetails(JSON.parse(userDetails))
+    }
+  })
   return (
     <Wrapper>
       <ChatSideBar />
