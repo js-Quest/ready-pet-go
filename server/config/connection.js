4992ad1a89f73e9aaaa173e0c35233dcc76d2131
Emma Carr <lorenagracex@gmail.com>
2023-06-14 14:18:21 -0500

    dog & cat box & hosted images w cloudinary

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,15 +2,13 @@ const mongoose = require("mongoose");
 
 // Wrap Mongoose around local connection to MongoDB
 
-mongoose.connect(
-  process.env.MONGO_URI ||
-    "mongodb+srv://lorenagracex:1989@practice.htuojqk.mongodb.net/chatAppDB",
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/chatAppDB', 
 
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
-);
+)
 
 // Export connection
 module.exports = mongoose.connection;
