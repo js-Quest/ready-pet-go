00c0812efddc2661268c97999b2bab1cd0dc6f8a
SunnyOhK <127900916+SunnyOhK@users.noreply.github.com>
2023-06-13 21:57:41 -0500

    almost forgot to revert back to original version of connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // Wrap Mongoose around local connection to MongoDB
-mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://SunnyOh:finn@practice.wchmv4y.mongodb.net/chatAppDB', 
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/chatAppDB',
 
   {
     useNewUrlParser: true,
