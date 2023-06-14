c0473400a37394808b28d2dc3ec5d89d6f562670
Emma Carr <lorenagracex@gmail.com>
2023-06-14 11:02:57 -0500

    PRODUCT PAGE BABY

---
+++ client/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: "JW Pet Chompion Dog Toy",
+      slug: "jw-pet-chompion-dog-toy",
+      category: "Dog Toy",
+      image: "/images/jwpetBone.jpg", // 679px × 829px
+      price: 11.99,
+      countInStock: 10,
+      brand: "JW Pet",
+      rating: 4,
+      numReviews: 10,
+      description:
+        "The idea of a dumbbell-shape has a lot to do with dogs you might call champion chewers. You and your dog will appreciate the Chompions durability and its textured nubs to help keep teeth clean, stimulate gums and promote oral health. An exceptional teething, fetch and tug toy.",
+    },
+    {
+      name: "JW Pet Chompion Dog Toy",
+      slug: "jw-pet-chompion-dog-toy",
+      category: "Dog Toy",
+      image: "/images/jwpetBone.jpg", // 679px × 829px
+      price: 11.99,
+      countInStock: 10,
+      brand: "JW Pet",
+      rating: 4,
+      numReviews: 10,
+      description:
+        "The idea of a dumbbell-shape has a lot to do with dogs you might call champion chewers. You and your dog will appreciate the Chompions durability and its textured nubs to help keep teeth clean, stimulate gums and promote oral health. An exceptional teething, fetch and tug toy.",
+    },
+    {
+      name: "JW Pet Chompion Dog Toy",
+      slug: "jw-pet-chompion-dog-toy",
+      category: "Dog Toy",
+      image: "/images/jwpetBone.jpg", // 679px × 829px
+      price: 11.99,
+      countInStock: 10,
+      brand: "JW Pet",
+      rating: 4,
+      numReviews: 10,
+      description:
+        "The idea of a dumbbell-shape has a lot to do with dogs you might call champion chewers. You and your dog will appreciate the Chompions durability and its textured nubs to help keep teeth clean, stimulate gums and promote oral health. An exceptional teething, fetch and tug toy.",
+    },
+    {
+      name: "JW Pet Chompion Dog Toy",
+      slug: "jw-pet-chompion-dog-toy",
+      category: "Dog Toy",
+      image: "/images/jwpetBone.jpg", // 679px × 829px
+      price: 11.99,
+      countInStock: 10,
+      brand: "JW Pet",
+      rating: 4,
+      numReviews: 10,
+      description:
+        "The idea of a dumbbell-shape has a lot to do with dogs you might call champion chewers. You and your dog will appreciate the Chompions durability and its textured nubs to help keep teeth clean, stimulate gums and promote oral health. An exceptional teething, fetch and tug toy.",
+    },
+  ],
+};
+export default data;
