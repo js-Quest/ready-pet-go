8066a92010772083832dd8af3340c28fc631a595
JASaddington <jasaddington@gmail.com>
2023-06-14 12:00:31 -0500

    changed styling for /firebase page and chat for mobile use

--- client/src/components/FirebaseChat/Welcome.js
+++ client/src/components/FirebaseChat/Welcome.js
@@ -9,13 +9,20 @@ const Welcome = () => {
     signInWithRedirect(auth, provider);
   };
 
+  const styles = {
+    img: {
+      height: "45px"
+    }
+  }
+
   return (
     <main className="welcome">
-      <h2>Welcome to PuppyChat!</h2>
+      <h2 className="welcome-message">Welcome to PuppyChat!</h2>
       {/* <img src="/logo512.png" alt="ReactJs logo" width={50} height={50} /> */}
       <p>Sign in with Google to chat with with your fellow Pet Enthusiasts!</p>
       <button className="sign-in">
         <img
+          style={styles.img}
           onClick={googleSignIn}
           src={GoogleSignin}
           alt="sign in with google"
