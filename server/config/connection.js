04c39b65c708569d38ac5bef9bc2132e88ac9312
Ki Sar <rxs291@yahoo.com>
2023-06-15 10:29:55 -0500

    pushing working quiz to main

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,7 @@ const mongoose = require("mongoose");
 
 
 mongoose.connect(
-  process.env.MONGO_URI ||
-    "mongodb+srv://rxs291:Never5660217!@classactivities.myu493z.mongodb.net/chatAppDB",
+  process.env.MONGO_URI || "",
 
   {
     useNewUrlParser: true,
