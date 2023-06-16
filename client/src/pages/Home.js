0b39d6ff34907d919ac3e06c4365236fe67a3367
SunnyOhK <127900916+SunnyOhK@users.noreply.github.com>
2023-06-15 22:57:49 -0500

    added a photo for healthcare section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,6 +11,7 @@ import Support from '../images/support.png';
 import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
 import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import HealthPet from '../images/health.png';
 
 export default function Home() {
   return (
@@ -25,9 +26,9 @@ export default function Home() {
         <Typography variant='h3' sx={{ backgroundColor: '#ffffff9f', width: '14em', textAlign: 'center', color: '#36393F', fontSize: '58px', paddingTop: '5px', paddingBottom: '5px' }}>Welcome Fellow Pet People! </Typography>
       </Box>
 
-        <div style={{ display: 'flex', marginLeft: 'auto', marginRight: 'auto', textAlign: 'center', justifyContent: 'center', alignItems: 'center', paddingBottom: '1em' }}>
+      <div style={{ display: 'flex', marginLeft: 'auto', marginRight: 'auto', textAlign: 'center', justifyContent: 'center', alignItems: 'center', paddingBottom: '1em' }}>
         <Typography variant='h5' sx={{ width: '70%', textAlign: 'center', color: '#36393F', paddingTop: '5px', paddingBottom: '5px', fontSize: '26px' }}>At Ready, Pet, Go!, our mission is simple: <i>to be a trusted companion for pet owners</i>. <br></br>We provide comprehensive guidance and support to prospective, new, and long-time pet parents.</Typography>
-        </div>
+      </div>
 
       <Box sx={{
         flexGrow: 1,
@@ -54,7 +55,7 @@ export default function Home() {
               • <br></br>
               • <br></br>
               • <br></br>
-              </Typography>
+            </Typography>
           </Grid>
 
           <Grid item xs={3.2} textAlign='center'
@@ -87,21 +88,21 @@ export default function Home() {
             <Typography variant='h5' sx={{ color: '#36393F', fontSize: '30px' }}>Community</Typography>
             <Typography sx={{ color: '#36393F', fontSize: '22px', paddingTop: '14px', marginBottom: '1.5em', paddingLeft: '12px', paddingRight: '12px', }}>We foster connections and provide a safe space where pet owners can come together to share their stories, seek advice, and celebrate the unconditional love that pets bring to our lives. <br></br>
               • <br></br>
-              </Typography>
+            </Typography>
           </Grid>
         </Grid>
       </Box>
 
-<Box sx={{ marginTop: '2.8em', width: '90%', marginLeft: 'auto', marginRight: 'auto'}}>
-  <Typography><FavoriteIcon sx={{fontSize: '60px'}}/></Typography>
-  <br></br>
-        <Typography><HealthAndSafetyIcon sx={{ fontSize: '60px' }} /></Typography>
-  <br></br>
-        <Typography><CardGiftcardIcon sx={{ fontSize: '60px' }} /></Typography>
-  <br></br>
-  
-</Box>
-      <div class="col-9 container-fluid mt-5 border border-5 rounded">
+      <Box sx={{ marginTop: '2.8em', width: '90%', marginLeft: 'auto', marginRight: 'auto' }}>
+        <Typography><FavoriteIcon sx={{ fontSize: '60px', color: '#CD7672' }} /></Typography>
+        <br></br>
+        <Typography><HealthAndSafetyIcon sx={{ fontSize: '60px', color: '#EEB462' }} /></Typography>
+        <br></br>
+        <Typography><CardGiftcardIcon sx={{ fontSize: '60px', color: '#534666' }} /></Typography>
+        <br></br>
+
+      </Box>
+      {/* <div class="col-9 container-fluid mt-5 border border-5 rounded">
         <p>
           We are dedicated to fostering a harmonious relationship between pets and their human companions by offering a wealth of reliable information, resources, and a vibrant community.
         </p>
@@ -157,7 +158,7 @@ export default function Home() {
             </span>
           </Link>
         </p>
-      </div>
+      </div> */}
 
     </Container>
 
