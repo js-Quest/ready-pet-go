8d1766ddf6c37567624dee82db6b8e85962c453c
SunnyOhK <127900916+SunnyOhK@users.noreply.github.com>
2023-06-16 19:46:00 -0500

    moved add pet form and pet card moved from dashboard into Profile. Has not yet been formatted but all testing for functionality has been successful

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, Navigate, useParams } from 'react-router-dom';
-// import PetCard from '../components/PetCard';
-// import PetForm from '../components/PetForm';
-// import PetButton from '../components/PetButton';
+import PetCard from '../components/PetCard';
+import PetForm from '../components/PetForm';
+import PetButton from '../components/PetButton';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
+// import CardContent from '@mui/material/CardContent';
 import UploadWidget from '../components/UploadWidget';
 import Auth from '../utils/auth';
 import { useMutation } from '@apollo/client';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import { UPDATE_USER } from '../utils/mutations';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+
 import PetPeek1 from '../images/peeking1.png';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
@@ -27,22 +27,32 @@ import ElGato from '../images/cat1.png';
 export default function Profile() {
   const [editProf, editProfile] = useState(false);
   const [profilePicture, setProfilePicture] = useState('')
-  const [formState, setFormState] = useState({ email: '', city: '', bio: '', username: '', phoneNumber: '' }); 
+  const [formState, setFormState] = useState({ email: '', city: '', bio: '', username: '', phoneNumber: '' });
   const [updateUser, { error }] = useMutation(UPDATE_USER);
-
-
+  const [petData, setPetData] = useState([]);
+  const [petArray, setPetArray] = useState([]);
+  const [numCard, setNumCard] = useState(0);
   const { username: userParam } = useParams();
 
+  
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
+  
+  useEffect(() => {
+    user?.pets ? setPetData(user.pets) : setPetArray([]);
+    console.log(user)
+  }, [data])
+  useEffect(() => {
+    console.log(petData)
+  }, [petData])
 
   const handleLoginError = () => {
     window.location.replace('/login');
   }
   console.log(data)
 
-  if (!data){
+  if (!data) {
     return (
       <Container className='errorContainer'>
         <h1 className='errorHead'>Hold Up!</h1>
@@ -71,13 +81,12 @@ export default function Profile() {
       </Container>
     )
   }
-  
+
   const user = data?.me || data?.user || {};
   // navigate to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Navigate to="/me" />; 
+    return <Navigate to="/me" />;
   }
- 
 
   if (loading) {
     return <div>Loading...</div>;
@@ -142,7 +151,6 @@ export default function Profile() {
   console.log(userParam)
   console.log(data)
   console.log(user)
- 
 
   return (
     <div>
@@ -153,7 +161,7 @@ export default function Profile() {
         />
       </div>
       <Container className='dashboardContainer'>
-        <h1 className='dashboardHead'>{userParam? userParam:user.username}'s Profile</h1>
+        <h1 className='dashboardHead'>{userParam ? userParam : user.username}'s Profile</h1>
 
         {/* HUMAN INFO */}
         <Box sx={{
@@ -255,7 +263,7 @@ export default function Profile() {
                       type="text"
                       value={formState.bio || user.bio}
                       onChange={handleChange}
-                      sx={{ width: { xs: '100%', md: '90%', lg: '95%',  } }}
+                      sx={{ width: { xs: '100%', md: '90%', lg: '95%', } }}
                     />
                   </Grid>
                   <Grid item xs={12} sx={{ textAlign: 'center', marginTop: '1.5em', marginBottom: '0.5em' }}>
@@ -285,100 +293,103 @@ export default function Profile() {
             (
               <Box maxWidth={'100%'} sx={{ paddingLeft: '2.8em', paddingRight: '2.8em' }}>
                 <br />
-                <Grid container spacing={0}> 
-                <Grid item xs={12} md={9}>
-                <Typography sx={{ fontSize: '1.5rem' }}>Username :</Typography>
-                <Typography sx={{ paddingLeft: '1em', fontSize: '1.2rem' }}>{user.username}</Typography>
-                <hr
-                  className='hr1'
-                  style={{
-                    background: "#CD7672",
-                    height: "2px",
-                    width: '40%',
-                    border: "none",
-                    marginTop: '0.4em',
-                    marginBottom: '1.3em',
-                  }}
-                />
-                <Typography sx={{ fontSize: '1.5rem' }}>Email :</Typography>
-                <Typography sx={{ paddingLeft: '1em', fontSize: '1.2rem' }}>{user.email}</Typography>
-                <hr
-                  className='hr2'
-                  style={{
-                    background: "#534666",
-                    height: "2px",
-                    width: '55%',
-                    border: "none",
-                    marginTop: '0.4em',
-                    marginBottom: '1.3em',
-                  }}
-                />
-                <Typography sx={{ fontSize: '1.5rem' }}>City, ST :</Typography>
-                <Typography sx={{ paddingLeft: '1em', fontSize: '1.2rem' }}>{user.city}</Typography>
-                <hr
-                  className='hr3'
-                  style={{
-                    background: "#DC8665",
-                    height: "2px",
-                    width: '70%',
-                    border: "none",
-                    marginTop: '0.4em',
-                    marginBottom: '1.3em',
-                  }}
-                />
-                <Typography sx={{ fontSize: '1.5rem' }}>Phone Number :</Typography>
-                <Typography sx={{ paddingLeft: '1em', fontSize: '1.2rem' }}>{user.phoneNumber}</Typography>
-                <hr
-                  className='hr4'
-                  style={{
-                    background: "#138086",
-                    height: "2px",
-                    width: '85%',
-                    border: "none",
-                    marginTop: '0.4em',
-                    marginBottom: '1.3em',
-                  }}
-                />
-                <Typography sx={{ fontSize: '1.5rem' }}>My Bio :</Typography>
-                <Typography sx={{ paddingLeft: '1em', fontSize: '1.2rem' }}>{user.bio}</Typography>
-                {/* view-only version of the profile picture is contained in this Card component */}
+                <Grid container spacing={0}>
+                  <Grid item xs={12} md={9}>
+                    <Typography sx={{ fontSize: '1.5rem' }}>Username :</Typography>
+                    <Typography sx={{ paddingLeft: '1em', fontSize: '1.2rem' }}>{user.username}</Typography>
+                    <hr
+                      className='hr1'
+                      style={{
+                        background: "#CD7672",
+                        height: "2px",
+                        width: '40%',
+                        border: "none",
+                        marginTop: '0.4em',
+                        marginBottom: '1.3em',
+                      }}
+                    />
+                    <Typography sx={{ fontSize: '1.5rem' }}>Email :</Typography>
+                    <Typography sx={{ paddingLeft: '1em', fontSize: '1.2rem' }}>{user.email}</Typography>
+                    <hr
+                      className='hr2'
+                      style={{
+                        background: "#534666",
+                        height: "2px",
+                        width: '55%',
+                        border: "none",
+                        marginTop: '0.4em',
+                        marginBottom: '1.3em',
+                      }}
+                    />
+                    <Typography sx={{ fontSize: '1.5rem' }}>City, ST :</Typography>
+                    <Typography sx={{ paddingLeft: '1em', fontSize: '1.2rem' }}>{user.city}</Typography>
+                    <hr
+                      className='hr3'
+                      style={{
+                        background: "#DC8665",
+                        height: "2px",
+                        width: '70%',
+                        border: "none",
+                        marginTop: '0.4em',
+                        marginBottom: '1.3em',
+                      }}
+                    />
+                    <Typography sx={{ fontSize: '1.5rem' }}>Phone Number :</Typography>
+                    <Typography sx={{ paddingLeft: '1em', fontSize: '1.2rem' }}>{user.phoneNumber}</Typography>
+                    <hr
+                      className='hr4'
+                      style={{
+                        background: "#138086",
+                        height: "2px",
+                        width: '85%',
+                        border: "none",
+                        marginTop: '0.4em',
+                        marginBottom: '1.3em',
+                      }}
+                    />
+                    <Typography sx={{ fontSize: '1.5rem' }}>My Bio :</Typography>
+                    <Typography sx={{ paddingLeft: '1em', fontSize: '1.2rem' }}>{user.bio}</Typography>
+                    {/* view-only version of the profile picture is contained in this Card component */}
 
-                <hr
-                  className='hr5'
-                  style={{
-                    background: "#EEB462",
-                    height: "2px",
-                    width: '100%',
-                    border: "none",
-                    marginTop: '0.4em',
-                    marginBottom: '1.3em',
-                  }}
-                />
+                    <hr
+                      className='hr5'
+                      style={{
+                        background: "#EEB462",
+                        height: "2px",
+                        width: '100%',
+                        border: "none",
+                        marginTop: '0.4em',
+                        marginBottom: '1.3em',
+                      }}
+                    />
                   </Grid>
                   <Grid item xs={12} md={3}>
-                    <Card sx={{ 
-                      width: {xs: 200, xl: 250}, 
+                    <Card sx={{
+                      width: { xs: 200, xl: 250 },
                       height: { xs: 200, xl: 250 },
                       marginLeft: { xs: '25%', md: '-100px', lg: '-50px', xl: '-20px' },
-                      }}>
-                    <CardMedia
+                    }}>
+                      <CardMedia
                         sx={{ height: { xs: 200, xl: 250 } }}
-                      image={user.profilePicture} />
-                  </Card>
+                        image={user.profilePicture} />
+                    </Card>
                   </Grid>
                 </Grid>
                 {data.me &&
-                <Box sx={{ 
-                  display: 'flex', 
-                  alignItems: 'center', 
-                  justifyContent: 'space-between',
-                  marginTop: { xs: '15px', lg: '0px'} }}>
-                  <Button variant='contained' type="click" onClick={editFormState} sx={{ backgroundColor: '#36393F', width: '9rem', marginLeft: 'auto' }}>Update</Button>
-                </Box>
+                  <Box sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    marginTop: { xs: '15px', lg: '0px' }
+                  }}>
+                    <Button variant='contained' type="click" onClick={editFormState} sx={{ backgroundColor: '#36393F', width: '9rem', marginLeft: 'auto' }}>Update</Button>
+                  </Box>
                 }
               </Box>
             )}
         </Box>
+
+        {/* PET INFO */}
 
         <Box sx={{
           border: '5px solid',
@@ -403,7 +414,7 @@ export default function Profile() {
             My Pets
           </Typography>
 
-          <Typography
+          <Box
             variant="body1"
             component="p"
             sx={{
@@ -415,18 +426,24 @@ export default function Profile() {
               paddingBottom: '3em',
             }}
           >
-            Jean shorts JOMO YOLO VHS marfa ugh, thundercats scenester cliche tote bag unicorn fit occupy. Leggings fashion axe cloud bread, chia green juice copper mug hashtag cardigan taxidermy meditation hell of. Shabby chic disrupt vaporware, hell of paleo you probably haven't heard of them pinterest migas. Meggings franzen hexagon fam marfa. You probably haven't heard of them taiyaki artisan banh mi echo park. Gorpcore hammock enamel pin plaid trust fund venmo. Activated charcoal YOLO gastropub tilde marfa. 
-            <br></br>
-            <br></br>
-            Jean shorts JOMO YOLO VHS marfa ugh, thundercats scenester cliche tote bag unicorn fit occupy. Leggings fashion axe cloud bread, chia green juice copper mug hashtag cardigan taxidermy meditation hell of. Shabby chic disrupt vaporware, hell of paleo you probably haven't heard of them pinterest migas. Meggings franzen hexagon fam marfa. You probably haven't heard of them taiyaki artisan banh mi echo park. Gorpcore hammock enamel pin plaid trust fund venmo. Activated charcoal YOLO gastropub tilde marfa.
-          </Typography>
-        <div className='catReach'>
-          <img
-            src={ElGato}
-            alt='CatSneezins'
-            style={{ marginLeft: '-8em', marginBottom: '-.4em'}}
-          />
-        </div>
+            {petData.map((item, i) => <PetCard pet={item} petData={petData} setPetData={setPetData} key={i} />)}
+            {/* array to render new PetForm whenever PetButton is clicked */}
+            {[...Array(numCard)].map((_, i) => <PetForm petData={petData} setPetData={setPetData} key={i} setShowCard={setNumCard} />)}
+
+            <PetButton 
+              setShowCard={setNumCard}
+              variant='text'
+              className='changeBtn' 
+              />
+
+          </Box>
+          <div className='catReach'>
+            <img
+              src={ElGato}
+              alt='CatSneezins'
+              style={{ marginLeft: '-8em', marginBottom: '-.4em' }}
+            />
+          </div>
         </Box>
       </Container >
     </div>
