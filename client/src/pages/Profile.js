1c6a1a2632449b593864ec9bfba170ef9ce0cdc3
JASaddington <jasaddington@gmail.com>
2023-06-16 19:57:36 -0500

    moved a line in profile.js because user was not being declared before the useEffect

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -38,7 +38,7 @@ export default function Profile() {
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
-  
+  const user = data?.me || data?.user || {};
   useEffect(() => {
     user?.pets ? setPetData(user.pets) : setPetArray([]);
     console.log(user)
@@ -82,7 +82,7 @@ export default function Profile() {
     )
   }
 
-  const user = data?.me || data?.user || {};
+
   // navigate to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Navigate to="/me" />;
